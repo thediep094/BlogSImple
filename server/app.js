f34122d988e4d20555212c96027b2bd335712c0b
thediep094 <thediep094@gmail.com>
2022-06-19 15:05:13 +0700

    Add files via upload

---
+++ server/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server');
+const mongoose = require('mongoose');
+const multer  = require('multer')
+const typeDefs = require('./schema/schema');
+const resolvers = require('./resolver/resolvers');
+const mongoDataMethods = require('./data/data')
+
+
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: () => ({ mongoDataMethods })
+});
+
+const connectDB = async () => {
+	try {
+		await mongoose.connect(`mongodb+srv://thediep094:diep0904@blog.zkgbj.mongodb.net/?retryWrites=true&w=majority`)
+		console.log('MongoDB connected sucessfully')
+	} catch (error) {
+		console.log(error.message)
+		process.exit(1)
+	}
+}
+
+connectDB()
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
